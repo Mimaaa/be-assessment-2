a29e85971ee4e7e330ca54f8af9bd843a15530fb
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2018-04-04 16:42:53 +0200

    Set view engine

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const express = require('express')
 const dotenv = require('dotenv').config();
 
 const app = express()
+  .set('view engine', 'ejs')
+  .set('views', 'view')
   .listen(process.env.PORT || 1902)
