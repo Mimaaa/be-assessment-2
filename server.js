737cb5a99d2e91848bf96bf70eb1aff025a3be25
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2018-04-04 16:49:54 +0200

    Connect home route

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ const dotenv = require('dotenv').config();
 const app = express()
   .set('view engine', 'ejs')
   .set('views', 'view')
+  .get('/', home)
   .listen(process.env.PORT || 1902)
+
+
+function home(req, res) {
+  res.render('home')
+}
