361e463fbc13fefa85b094fb86efdbda5328d17e
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2018-04-06 15:55:50 +0200

    Rewrite functions to promise standards

--- server.js
+++ server.js
@@ -28,64 +28,56 @@ const app = express()
 function home(req, res) {
   const result = { errors: [], data: undefined }
 
-  try {
-    const client = new Client()
-    client.connect()
-      .then(() => {
-        console.log('connection complete');
+  const client = new Client()
+  client.connect()
+    .then(() => {
+      console.log('connection complete');
 
-        const sql = 'SELECT * FROM lifters'
-        return client.query(sql)
+      const sql = 'SELECT * FROM lifters'
+      return client.query(sql)
+    })
+    .then((result) => {
+      result.data = result.rows
+      res.format({
+        json: () => { return res.json(result) },
+        html: () => { return res.render('home', result) }
       })
-      .then((result) => {
-        result.data = result.rows
-        res.format({
-          json: function () {
-            return res.json(result)
-          },
-          html: function () {
-            return res.render('home', result)
-          }
-        })
-      })
-  } catch (err) {
-    result.errors.push({ id: 400, title: 'bad request' })
-    res.status(400).render('error', result)
-    return
-  }
+    })
+    .catch((error) => {
+      console.log('er is een error')
+      // result.errors.push({ id: 400, title: 'bad request' })
+      // res.status(400).render('error', result)
+      // return
+    })
 }
 
 function get(req, res) {
   const id = req.params.id
   const result = { errors: [], data: undefined }
 
-  try {
-    const client = new Client()
-    client.connect()
-      .then(() => {
-        console.log('connection complete');
+  const client = new Client()
+  client.connect()
+    .then(() => {
+      console.log('Connected with DB');
 
-        const sql = 'SELECT * FROM lifters WHERE id = $1'
-        const params = [id]
-        return client.query(sql, params)
+      const sql = 'SELECT * FROM lifters WHERE id = $1'
+      const params = [id]
+      return client.query(sql, params)
+    })
+    .then((result) => {
+      console.log(result)
+      result.data = result.rows[0]
+      res.format({
+        json: () => { return res.json(result)},
+        html: () => { return res.render('detail', result)}
       })
-      .then((result) => {
-        result.data = result.rows[0]
-        res.format({
-          json: function () {
-            return res.json(result)
-          },
-          html: function () {
-            return res.render('detail', result)
-          }
-        })
-      })
-  } catch (err) {
-    console.log('er is een error')
-    result.errors.push({ id: 400, title: 'bad request' })
-    res.status(400).render('error', result)
-    return
-  }
+    })
+    .catch((error) => {
+      console.log('er is een error')
+      // result.errors.push({ id: 400, title: 'bad request' })
+      // res.status(400).render('error', result)
+      // return
+    })
 }
 
 function form(req, res) {
@@ -95,54 +87,52 @@ function form(req, res) {
 function add(req, res) {
   const result = { errors: [], data: undefined}
 
-  try {
-    const client = new Client()
-    client.connect()
-      .then(() => {
-        console.log('connection complete');
+  const client = new Client()
+  client.connect()
+    .then(() => {
+      console.log('connection complete');
 
-        const sql = 'INSERT INTO lifters (naam, geslacht, geboortedatum, lichaamsgewicht) VALUES ($1, $2, $3, $4) RETURNING id'
-        const params = [req.body.naam, req.body.geslacht, req.body.geboortedatum, +req.body.lichaamsgewicht]
-        return client.query(sql, params)
-      })
-      .then((result) => {
-        if (req.file) {
-          fs.rename(req.file.path, 'static/images/'+result.rows[0].id+'.jpg')
-        }
-        console.log(result)
-        res.redirect('/' + result.rows[0].id)
-      })
-  } catch (err) {
-    result.errors.push({ id: 422, title: 'unprocessable entity' })
-    res.status(422).render('error.ejs', result)
-    console.log(err)
-    return
-  }
+      const sql = 'INSERT INTO lifters (naam, geslacht, geboortedatum, lichaamsgewicht) VALUES ($1, $2, $3, $4) RETURNING id'
+      const params = [req.body.naam, req.body.geslacht, req.body.geboortedatum, +req.body.lichaamsgewicht]
+      return client.query(sql, params)
+    })
+    .then((result) => {
+      if (req.file) {
+        fs.rename(req.file.path, 'static/images/' + result.rows[0].id + '.jpg')
+      }
+      console.log(result)
+      res.redirect('/' + result.rows[0].id)
+    })
+    .catch((error) => {
+      console.log('er is een error')
+      // result.errors.push({ id: 400, title: 'bad request' })
+      // res.status(400).render('error', result)
+      // return
+    })
 }
 
 function remove(req, res) {
   const id = req.params.id
 
-  try {
-    const client = new Client()
-    client.connect()
-      .then(() => {
-        console.log('connection complete');
+  const client = new Client()
+  client.connect()
+    .then(() => {
+      console.log('connection complete');
 
-        const sql = 'DELETE FROM lifters WHERE id = $1'
-        const params = [id]
-        return client.query(sql, params)
-      })
-      .then((result) => {
-        console.log(result)
-        res.redirect('/')
-      })
-  } catch (err) {
-    console.log('er is een error')
-    result.errors.push({ id: 400, title: 'bad request' })
-    res.status(400).render('error', result)
-    return
-  }
+      const sql = 'DELETE FROM lifters WHERE id = $1'
+      const params = [id]
+      return client.query(sql, params)
+    })
+    .then((result) => {
+      console.log(result)
+      res.redirect('/')
+    })
+    .catch((error) => {
+      console.log('er is een error')
+      // result.errors.push({ id: 400, title: 'bad request' })
+      // res.status(400).render('error', result)
+      // return
+    })
 }
 
 // https://stackoverflow.com/questions/44171497/express-multer-filefilter-error-handling
