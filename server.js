af1858ebb99db1d962cd15d0850c5a18d4e6eb4f
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2018-04-07 14:13:37 +0200

    Install and require argon2

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const multer = require('multer')
 const fs = require('fs')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv').config();
+const argon2 = require('argon2')
 
 const upload = multer({
   dest: 'static/images'
