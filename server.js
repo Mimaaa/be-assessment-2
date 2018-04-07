4a8af4a9d64c1caeff53843d858cfa0cc622d8ba
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2018-04-07 16:30:34 +0200

    Fix linting errors

--- server.js
+++ server.js
@@ -1,206 +1,211 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const { Client } = require('pg')
-const multer = require('multer')
-const fs = require('fs')
-const bodyParser = require('body-parser')
+const fs = require('fs');
+const express = require('express');
+const {Client} = require('pg');
+const multer = require('multer');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 
 const upload = multer({
-  dest: 'static/images'
-})
+	dest: 'static/images'
+});
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-})
-client.connect()
+	connectionString: process.env.DATABASE_URL,
+	ssl: true
+});
+client.connect();
 console.log('Connected with DB');
 
 const app = express()
-  .use(express.static('static'))
-  .set('view engine', 'ejs')
-  .set('views', 'view')
-  .use(bodyParser.json())
-  .get('/', home)
-  .get('/add', addForm)
-  .get('/signup', signupForm)
-  .get('/login', loginForm)
-  .get('/:id', get)
-  .get('/edit/:id', editForm)
-  .post('/', upload.single('image'), add)
-  .post('/:id', remove)
-  .post('/edit/:id', upload.single('image'), edit)
-  .listen(process.env.PORT || 1902)
+	.use(express.static('static'))
+	.set('view engine', 'ejs')
+	.set('views', 'view')
+	.use(bodyParser.json())
+	.get('/', home)
+	.get('/add', addForm)
+	.get('/signup', signupForm)
+	.get('/login', loginForm)
+	.get('/:id', get)
+	.get('/edit/:id', editForm)
+	.post('/', upload.single('image'), add)
+	.post('/:id', remove)
+	.post('/edit/:id', upload.single('image'), edit)
+	.listen(process.env.PORT || 1902);
 
 function home(req, res) {
-  const result = { errors: [], data: undefined }
-  const sql = 'SELECT * FROM lifters ORDER BY id'
+	const result = {errors: [], data: undefined};
+	const sql = 'SELECT * FROM lifters ORDER BY id';
 
   client.query(sql)
-    .then((data) => {
-      if (data.rowCount === 0) {
-        result.errors.push({ id: 404, title: 'not found' })
-        res.status(404).render('error', result)
-        return
-      } else {
-        result.data = data.rows
+  	.then(data => {
+  		if (data.rowCount === 0) {
+        result.errors.push({id: 404, title: 'not found'});
+        res.status(404).render('error', result);
+  		} else {
+  			result.data = data.rows;
         res.format({
-          json: () => { return res.json(result) },
-          html: () => { return res.render('home', result) }
-        })
-      }
-    })
-    .catch((error) => {
-      result.errors.push({ id: 400, title: 'bad request' })
-      res.status(400).render('error', result)
-      return
-    })
+        	json: () => {
+        		return res.json(result);
+        	},
+        	html: () => {
+        		return res.render('home', result);
+        	}
+        });
+  		}
+  	})
+  	.catch(error => {
+      console.log(error)
+      result.errors.push({id: 400, title: 'bad request'});
+      res.status(400).render('error', result);
+  	});
 }
 
 function get(req, res) {
-  const result = { errors: [], data: undefined }
-  const sql = 'SELECT * FROM lifters WHERE id = $1'
-  const params = [req.params.id]
+	const result = {errors: [], data: undefined};
+	const sql = 'SELECT * FROM lifters WHERE id = $1';
+	const params = [req.params.id];
 
   client.query(sql, params)
-    .then((data) => {
-      if (data.rowCount === 0) {
-        result.errors.push({ id: 404, title: 'not found' })
-        res.status(404).render('error', result)
-        return
-      } else {
-        result.data = data.rows[0]
+  	.then(data => {
+  		if (data.rowCount === 0) {
+        result.errors.push({id: 404, title: 'not found'});
+        res.status(404).render('error', result);
+        return;
+  		}
+  			result.data = data.rows[0];
         res.format({
-          json: () => { return res.json(result) },
-          html: () => { return res.render('detail', result) }
-        })
-      }
-    })
-    .catch((error) => {
-      result.errors.push({ id: 400, title: 'bad request' })
-      res.status(400).render('error', result)
-      return
-    })
+        	json: () => {
+        		return res.json(result);
+        	},
+        	html: () => {
+        		return res.render('detail', result);
+        	}
+        });
+  	})
+  	.catch(error => {
+      console.log(error)
+      result.errors.push({id: 400, title: 'bad request'});
+      res.status(400).render('error', result);
+  	});
 }
 
 function addForm(req, res) {
-  res.render('add')
+  res.render('add');
 }
 
 function add(req, res) {
-  const result = { errors: [], data: undefined}
+	const result = {errors: [], data: undefined};
 
-  // const findLastId = 'SELECT MAX(id) FROM lifters'
-  // let lastId
+	// Const findLastId = 'SELECT MAX(id) FROM lifters'
+	// let lastId
 
-  // client.query(findLastId)
-  //   .then((data) => {
-  //     lastId = data.rows[0]
-  //   })
+	// client.query(findLastId)
+	//   .then((data) => {
+	//     lastId = data.rows[0]
+	//   })
 
-  const sql = `INSERT INTO lifters (naam, geslacht, geboortedatum, lichaamsgewicht) VALUES ($1, $2, $3, $4) RETURNING id`
-  const params = [req.body.naam, req.body.geslacht, req.body.geboortedatum, +req.body.lichaamsgewicht]
+	const sql = `INSERT INTO lifters (naam, geslacht, geboortedatum, lichaamsgewicht) VALUES ($1, $2, $3, $4) RETURNING id`;
+	const params = [req.body.naam, req.body.geslacht, req.body.geboortedatum, Number(req.body.lichaamsgewicht)];
 
   client.query(sql, params)
-    .then((data) => {
-      if (data.rowCount === 0) {
-        result.errors.push({ id: 422, title: 'unprocessable entity' })
-        res.status(422).render('error', result)
-        return
-      } else {
-        if (req.file) {
-          fs.rename(req.file.path, 'static/images/'+data.rows[0].id+'.jpg')
-        }
-        res.redirect('/' + data.rows[0].id)
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-      result.errors.push({ id: 400, title: 'bad request' })
-      res.status(400).render('error', result)
-      return
-    })
+  	.then(data => {
+  		if (data.rowCount === 0) {
+        result.errors.push({id: 422, title: 'unprocessable entity'});
+        res.status(422).render('error', result);
+  		} else {
+  			if (req.file) {
+          fs.rename(req.file.path, 'static/images/' + data.rows[0].id + '.jpg');
+  			}
+        res.redirect('/' + data.rows[0].id);
+  		}
+  	})
+  	.catch(error => {
+      console.log(error);
+      result.errors.push({id: 400, title: 'bad request'});
+      res.status(400).render('error', result);
+  	});
 }
 
 function remove(req, res) {
-  const result = { errors: [], data: undefined }
-  const sql = 'DELETE FROM lifters WHERE id = $1'
-  const params = [req.params.id]
+	const result = {errors: [], data: undefined};
+	const sql = 'DELETE FROM lifters WHERE id = $1';
+	const params = [req.params.id];
 
   client.query(sql, params)
-    .then((data) => {
-      if (data.rowCount === 0) {
-        result.errors.push({ id: 404, title: 'not found' })
-        res.status(404).render('error', result)
-        return
-      } else {
-        res.redirect('/')
-      }
-    })
-    .catch((error) => {
-      result.errors.push({ id: 400, title: 'bad request' })
-      res.status(400).render('error', result)
-      return
-    })
+  	.then(data => {
+  		if (data.rowCount === 0) {
+        result.errors.push({id: 404, title: 'not found'});
+        res.status(404).render('error', result);
+        return;
+  		}
+        res.redirect('/');
+  	})
+  	.catch(error => {
+      console.log(error)
+      result.errors.push({id: 400, title: 'bad request'});
+      res.status(400).render('error', result);
+  	});
 }
 
 function editForm(req, res) {
-  const result = { errors: [], data: undefined }
-  const sql = 'SELECT * FROM lifters WHERE id = $1'
-  const params = [req.params.id]
+	const result = {errors: [], data: undefined};
+	const sql = 'SELECT * FROM lifters WHERE id = $1';
+	const params = [req.params.id];
 
   client.query(sql, params)
-    .then((data) => {
-      if (data.rowCount === 0) {
-        result.errors.push({ id: 404, title: 'not found' })
-        res.status(404).render('error', result)
-        return
-      } else {
-        result.data = data.rows[0]
+  	.then(data => {
+  		if (data.rowCount === 0) {
+        result.errors.push({id: 404, title: 'not found'});
+        res.status(404).render('error', result);
+  		} else {
+  			result.data = data.rows[0];
         res.format({
-          json: () => { return res.json(result) },
-          html: () => { return res.render('edit', result) }
-        })
-      }
-    })
-    .catch((error) => {
-      result.errors.push({ id: 400, title: 'bad request' })
-      res.status(400).render('error', result)
-      return
-    })
+        	json: () => {
+        		return res.json(result);
+        	},
+        	html: () => {
+        		return res.render('edit', result);
+        	}
+        });
+  		}
+  	})
+  	.catch(error => {
+      console.log(error)
+      result.errors.push({id: 400, title: 'bad request'});
+      res.status(400).render('error', result);
+  	});
 }
 
 function edit(req, res) {
-  const result = { errors: [], data: undefined }
-  const sql = 'UPDATE lifters SET naam = $1, geslacht = $2, geboortedatum = $3, lichaamsgewicht = $4 WHERE id = $5'
-  const params = [req.body.naam, req.body.geslacht, req.body.geboortedatum, +req.body.lichaamsgewicht, req.params.id]
+	const result = {errors: [], data: undefined};
+	const sql = 'UPDATE lifters SET naam = $1, geslacht = $2, geboortedatum = $3, lichaamsgewicht = $4 WHERE id = $5';
+	const params = [req.body.naam, req.body.geslacht, req.body.geboortedatum, Number(req.body.lichaamsgewicht), req.params.id];
 
   client.query(sql, params)
-    .then((data) => {
-      if (data.rowCount === 0) {
-        result.errors.push({ id: 422, title: 'unprocessable entity' })
-        res.status(422).render('error', result)
-        return
-      } else {
-        if (req.file) {
-          fs.rename(req.file.path, 'static/images/'+req.params.id+'.jpg')
-        }
-        res.redirect('/' + req.params.id)
-      }
-    })
-    .catch((error) => {
-      result.errors.push({ id: 400, title: 'bad request' })
-      res.status(400).render('error', result)
-      return
-    })
+  	.then(data => {
+  		if (data.rowCount === 0) {
+        result.errors.push({id: 422, title: 'unprocessable entity'});
+        res.status(422).render('error', result);
+  		} else {
+  			if (req.file) {
+          fs.rename(req.file.path, 'static/images/' + req.params.id + '.jpg');
+  			}
+        res.redirect('/' + req.params.id);
+  		}
+  	})
+  	.catch(error => {
+      console.log(error)
+      result.errors.push({id: 400, title: 'bad request'});
+      res.status(400).render('error', result);
+  	});
 }
 
 function signupForm(req, res) {
-  res.render('signup')
+  res.render('signup');
 }
 
 function loginForm(req, res) {
-  res.render('login')
+  res.render('login');
 }
